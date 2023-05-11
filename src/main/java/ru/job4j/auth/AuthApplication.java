12bc174206d31d3304b9bdb2f6ae4327b12c11e9
Ilya <i.kaltygin@icloud.com>
2023-05-11 21:36:29 +0700

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/auth/AuthApplication.java
+++ src/main/java/ru/job4j/auth/AuthApplication.java
@@ -1,23 +1,40 @@
 package ru.job4j.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class AuthApplication {
+public class AuthApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
-	}
+    /**
+     * Создает бин BCryptPasswordEncoder
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	/**
-	 * Создает бин BCryptPasswordEncoder
-	 */
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    /**
+     * Создает бин SpringLiquibase
+     *
+     * @param ds источник данных (DataSource)
+     */
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db/dbchangelog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
 
 }
